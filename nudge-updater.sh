24113ddfe2fea61f6c477380fb96e35dfb19743f
Aaron Hodgson <aaron.hodgson.it@gmail.com>
2024-04-30 10:37:44 -0500

    Update nudge-updater.sh

--- nudge-updater.sh
+++ nudge-updater.sh
@@ -205,7 +205,7 @@ function calculate_previous_latest_versions(){
 	current_N1_major=$(echo "$current_N1" | cut -d '.' -f1)
 	current_N2_major=$(echo "$current_N2" | cut -d '.' -f1)
 	#-----------------------------------------------------------#
-	versions=$(jq -r '.osVersionRequirements[].[].requiredMinimumOSVersion' "$json_file")
+	versions=$(jq -r '.osVersionRequirements[].requiredMinimumOSVersion' "$json_file")
 	while IFS= read -r version
 	do
 		version_major=$(echo "$version" | cut -d '.' -f1)
@@ -436,40 +436,12 @@ function sort_rules(){
 #############################################################
 # MAIN
 #############################################################
-# clone_repo
-# get_latest_versions
-# if $json_file_updated; then
-# 	backup_json_files
-# 	update_repo "$version_json_file"
+clone_repo
+get_latest_versions
+if $json_file_updated; then
+	backup_json_files
+	update_repo "$version_json_file"
 
-# 	for current_file_name in ${json_files[@]}; do
-# 		json_file="$SCRIPT_DIR/json/$current_file_name.json"
-# 		echo "#############################################################"
-# 		echo "#############################################################"
-# 		echo "	Current JSON File = $current_file_name"
-# 		echo "#############################################################"
-# 		echo "#############################################################"
-# 		echo ""
-# 		echo ""
-# 	  	calculate_previous_latest_versions
-# 		calculate_new_deadline_dates
-# 		update_min_os_requirements
-# 		delete_expired_rules
-# 		create_new_deadline_rule
-# 		sort_rules
-# 		update_repo "$json_file"
-# 	done
-# 	commit_repo
-# else
-# 	echo "#############################################################"
-# 	echo "Nothing changed, skipping repo update"
-# fi
-# echo "#############################################################"
-# echo "DONE!"
-
-#############################################################
-#############################################################
-#############################################################
 	for current_file_name in ${json_files[@]}; do
 		json_file="$SCRIPT_DIR/json/$current_file_name.json"
 		echo "#############################################################"
@@ -478,11 +450,16 @@ function sort_rules(){
 		echo ""
 	  	calculate_previous_latest_versions
 		calculate_new_deadline_dates
-		# update_min_os_requirements
-		# delete_expired_rules
-		# create_new_deadline_rule
-		# sort_rules
-		# update_repo "$json_file"
+		update_min_os_requirements
+		delete_expired_rules
+		create_new_deadline_rule
+		sort_rules
+		update_repo "$json_file"
 	done
+	commit_repo
+else
+	echo "#############################################################"
+	echo "Nothing changed, skipping repo update"
+fi
 echo "#############################################################"
 echo "DONE!"
