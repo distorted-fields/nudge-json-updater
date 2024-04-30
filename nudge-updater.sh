b35ea6fc61e9e4733048b4291021e20c7c8a7297
Aaron Hodgson <aaron.hodgson.it@gmail.com>
2024-04-29 21:24:15 -0500

    Update nudge-updater.sh

--- nudge-updater.sh
+++ nudge-updater.sh
@@ -441,27 +441,27 @@ get_latest_versions
 if $json_file_updated; then
 	backup_json_files
 	update_repo "$version_json_file"
+	for current_file_name in ${json_files[@]}; do
+		json_file="$SCRIPT_DIR/json/$current_file_name.json"
+		echo "#############################################################"
+		echo "#############################################################"
+		echo "	Current JSON File = $current_file_name"
+		echo "#############################################################"
+		echo "#############################################################"
+		echo ""
+		echo ""
+	  	calculate_previous_latest_versions
+		calculate_new_deadline_dates
+		update_min_os_requirements
+		delete_expired_rules
+		create_new_deadline_rule
+		sort_rules
+		update_repo "$json_file"
+	done
+	commit_repo
 else
 	echo "#############################################################"
 	echo "Nothing changed, skipping repo update"
 fi
-for current_file_name in ${json_files[@]}; do
-	json_file="$SCRIPT_DIR/json/$current_file_name.json"
-	echo "#############################################################"
-	echo "#############################################################"
-	echo "	Current JSON File = $current_file_name"
-	echo "#############################################################"
-	echo "#############################################################"
-	echo ""
-	echo ""
-  	calculate_previous_latest_versions
-	calculate_new_deadline_dates
-	update_min_os_requirements
-	delete_expired_rules
-	create_new_deadline_rule
-	sort_rules
-	update_repo "$json_file"
-done
-commit_repo
 echo "#############################################################"
 echo "DONE!"
